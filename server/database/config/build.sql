0d3a9b1cdef09e840da58226e72e6d1bad9e7469
MaiUbeid <ubead95@gmail.com>
2019-09-06 13:21:17 +0300

    add users table to database then code build.js

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS city cascade;
+DROP TABLE IF EXISTS city,users cascade;
 
 CREATE TABLE city (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -8,9 +8,17 @@ CREATE TABLE city (
   country TEXT NOT NULL
 );
 
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR(20) UNIQUE NOT NULL,
+  user_password VARCHAR(30)
+);
+
 INSERT INTO city (name, country) VALUES
   ('Gaza', 'Palestine'),
   ('London', 'UK'),
   ('New York', 'USA');
+
+INSERT INTO users (email, user_password) VALUES ('mai@gmail.com', '123gaza');
 
 COMMIT;

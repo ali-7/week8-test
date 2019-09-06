f1a1ff60c75d88dba32f1ed990a18aa798a99e2b
MaiUbeid <ubead95@gmail.com>
2019-09-06 20:39:51 +0300

    edit getUser to take the email

--- server/database/queries/getUser.js
+++ server/database/queries/getUser.js
@@ -1,8 +1,12 @@
 // Write a query to get the user and their password from the database
 const dbConnection = require('../config/connection');
 
-const getUser = () => {
-  const sql = 'SELECT * FROM users;';
+const getUser = userData => {
+  const { email } = userData;
+  const sql = {
+    text: 'SELECT user_password FROM users WHERE email = ($1);',
+    value: [email]
+  };
   return dbConnection.query(sql);
 };
 

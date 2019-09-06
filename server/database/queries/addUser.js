cd9c603ac095b1881819569ccb3b44dc82f5cde3
MaiUbeid <ubead95@gmail.com>
2019-09-06 13:27:48 +0300

    Build getUser and addUser query

--- server/database/queries/addUser.js
+++ server/database/queries/addUser.js
@@ -1,1 +1,13 @@
 // Write a query to add the user and their password to the database
+const dbConnection = require('../config/connection');
+
+const addUser = userData => {
+  const { email, password } = userData;
+  const sql = {
+    text: 'INSERT INTO users (email, user_password) VALUES ($1, $2);',
+    values: [email, password]
+  };
+  return dbConnection.query(sql);
+};
+
+module.exports = { addUser };
--- server/database/queries/getUser.js
+++ server/database/queries/getUser.js
@@ -1,1 +1,9 @@
 // Write a query to get the user and their password from the database
+const dbConnection = require('../config/connection');
+
+const getUser = () => {
+  const sql = 'SELECT * FROM users;';
+  return dbConnection.query(sql);
+};
+
+module.exports = { getUser };

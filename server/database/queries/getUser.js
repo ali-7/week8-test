29b3615c5c93ba53b48d35a1d6a954efc4fbb9c0
FadiAlamassi <alamassifadi@gmail.com>
2019-09-06 11:50:04 +0300

    query to get  user password from the database

--- server/database/queries/getUser.js
+++ server/database/queries/getUser.js
@@ -1,1 +1,10 @@
 // Write a query to get the user and their password from the database
+const dbConnection = require('../config/connection');
+
+const getUser = (email) => {
+  return dbConnection.query(`SELECT password FROM user;`);
+};
+
+module.exports = {
+  getUser,
+};

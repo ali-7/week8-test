0f3c2d7a4b8ae38ec18e837ad2dbec6a16d6f40a
Asmaathabet <asmathabet2009@hotmail.com>
2019-09-06 09:53:57 +0300

    add users table to build.sql file

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,11 +1,17 @@
 BEGIN;
 
-DROP TABLE IF EXISTS city cascade;
+DROP TABLE IF EXISTS city,users cascade;
 
 CREATE TABLE city (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
   country TEXT NOT NULL
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR(255) UNIQUE , 
+  password VARCHAR(255)
 );
 
 INSERT INTO city (name, country) VALUES

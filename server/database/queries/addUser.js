e7a0868b3346dba054f6337d6a6c8bdae051378f
FadiAlamassi <alamassifadi@gmail.com>
2019-09-06 11:49:38 +0300

    query to add user to the database

--- server/database/queries/addUser.js
+++ server/database/queries/addUser.js
@@ -1,1 +1,11 @@
 // Write a query to add the user and their password to the database
+// Write a query to add the user and their password to the database
+const connection = require('../config/connection');
+
+exports.addUser = (email, password) => {
+  const sql = {
+    text: 'INSERT INTO user(email,password) VALUES($1,$2)',
+    valules: [email, password]
+  }
+  return connection.query(sql);
+}

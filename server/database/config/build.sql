26193a9b56ca98e10704557a41ff67754b54db79
amoodaa <amoodaa@gmail.com>
2019-09-07 15:03:05 +0300

    added the user table and formatted the connection.js file

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,16 +1,16 @@
 BEGIN;
-
-DROP TABLE IF EXISTS city cascade;
-
+DROP TABLE IF EXISTS city CASCADE;
 CREATE TABLE city (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
   country TEXT NOT NULL
 );
-
-INSERT INTO city (name, country) VALUES
-  ('Gaza', 'Palestine'),
-  ('London', 'UK'),
-  ('New York', 'USA');
-
+CREATE TABLE test_user (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  PASSWORD VARCHAR(255) NOT NULL
+);
+INSERT INTO city (name, country)
+  VALUES ('Gaza', 'Palestine'), ('London', 'UK'), ('New York', 'USA');
 COMMIT;
+

f0aadc53bc45caad62fe6d4daf14711f8dc14d60
Mohammed <mhmmade@gmail.com>
2019-09-10 01:12:05 +0300

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const { join } = require("path");
-const router = require("./controllers");
+const express = require('express');
+const { join } = require('path');
+const router = require('./controllers');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.set('port', process.env.PORT || 5000);
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(join(__dirname, "..", "public")));
-
-app.set("port", process.env.PORT || 5000);
-
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use(router);
 
 module.exports = app;
--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,16 +1,23 @@
+
 BEGIN;
-
-DROP TABLE IF EXISTS city cascade;
-
-CREATE TABLE city (
+  DROP TABLE IF EXISTS city, test_user
+  CASCADE;
+CREATE TABLE city
+(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
   country TEXT NOT NULL
 );
-
-INSERT INTO city (name, country) VALUES
+CREATE TABLE test_user
+(
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  PASSWORD VARCHAR(255) NOT NULL
+);
+INSERT INTO city
+  (name, country)
+VALUES
   ('Gaza', 'Palestine'),
   ('London', 'UK'),
   ('New York', 'USA');
-
 COMMIT;
